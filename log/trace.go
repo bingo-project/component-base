62df0e280c163cdae8c40947ae694393293025e7
Brooks <brooksyang@outlook.com>
2024-02-09 11:46:54 +0800

    feat: update log

--- log/trace.go
+++ log/trace.go
@@ -7,8 +7,17 @@ import (
 )
 
 var (
-	XRequestIDKey = "X-Request-ID"
-	XUsernameKey  = "X-Username"
+	KeyTrace    = "trace"
+	KeySubject  = "subject"
+	KeyObject   = "object"
+	KeyInstance = "instance"
+	KeyTask     = "task"
+	KeyCost     = "cost"
+	KeyResult   = "result"
+	KeyStatus   = "status"
+	KeyIP       = "ip"
+	KeyInfo     = "info"
+	KeyWatcher  = "watcher"
 )
 
 // C Parse context.
@@ -19,12 +28,48 @@ func C(ctx context.Context) *zapLogger {
 func (l *zapLogger) C(ctx context.Context) *zapLogger {
 	lc := l.clone()
 
-	if requestID := ctx.Value(XRequestIDKey); requestID != nil {
-		lc.z = lc.z.With(zap.Any(XRequestIDKey, requestID))
+	if data := ctx.Value(KeyTrace); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyTrace, data))
 	}
 
-	if userID := ctx.Value(XUsernameKey); userID != nil {
-		lc.z = lc.z.With(zap.Any(XUsernameKey, userID))
+	if data := ctx.Value(KeySubject); data != nil {
+		lc.z = lc.z.With(zap.Any(KeySubject, data))
+	}
+
+	if data := ctx.Value(KeyObject); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyObject, data))
+	}
+
+	if data := ctx.Value(KeyInstance); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyInstance, data))
+	}
+
+	if data := ctx.Value(KeyTask); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyTask, data))
+	}
+
+	if data := ctx.Value(KeyCost); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyCost, data))
+	}
+
+	if data := ctx.Value(KeyResult); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyResult, data))
+	}
+
+	if data := ctx.Value(KeyStatus); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyStatus, data))
+	}
+
+	if data := ctx.Value(KeyIP); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyIP, data))
+	}
+
+	if data := ctx.Value(KeyInfo); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyInfo, data))
+	}
+
+	if data := ctx.Value(KeyWatcher); data != nil {
+		lc.z = lc.z.With(zap.Any(KeyWatcher, data))
 	}
 
 	return lc
